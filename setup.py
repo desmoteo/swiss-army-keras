2bde8db22df074c91fa0a4e9c9b1daea7453fef2
Matteo Ferrabone <matteo.ferrabone@gmail.com>
2021-10-27 12:09:41 +0200

    Added setup, renamed project

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='swiss-army-keras',
+      version = '0.1.0',
+      description = 'A collection of models and utilities for the development of edge deployable Keras models',
+      long_description = long_description,
+      keywords = ['keras', 'segmentation', 'edge', 'quantization', 'augmentation'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'License :: OSI Approved :: MIT License',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Programming Language :: Python :: 3.6',
+          'Topic :: Multimedia :: Graphics'],
+      url='http://github.com/desmoteo/swiss-army-keras',
+      author='Matteo Ferrabone',
+      author_email='matteo.ferrabone@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires = ['numpy', 'tensorflow'],
+      zip_safe = False)
